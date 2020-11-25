c9e3f53405a2aaafaca99fb1d13d4e251599903a
Hiromi00 <55908313+hiromi00@users.noreply.github.com>
2020-11-24 22:25:56 -0600

    Inicializacion del backend #2

---
+++ backend/routes/geis.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+
+let geis = require("../temporalDatabase");
+
+router.get("/list", async (req, res) => {
+    try {
+      res.status(200).json({
+        data: geis
+      });
+    } catch (err) {
+      res.status(400).json({
+        message: "Some error occured",
+        err
+      });
+    }
+  });
+
+
+  module.exports = router;
