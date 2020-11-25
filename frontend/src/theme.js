5a0f4164af2dc3c7a99701a41829dafac8a71ea5
EduardoPEMA <eduardopro00@gmail.com>
2020-11-25 01:51:42 -0600

    Se agrega el material-ui y el theme.js issue #14

---
+++ frontend/src/theme.js
@@ -0,0 +1,19 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: "#c44cff",
+      main: "#8360c3",
+      dark: "#5700ad",
+    },
+    secondary: {
+      light: "#6cf2c1",
+      main: "#2ebf91",
+      dark: "#008e63",
+    },
+  },
+});
+
+export default theme;
+
