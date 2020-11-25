7e03f3670fec30792c3ee280c36768e8dbfbcb37
EduardoPEMA <eduardopro00@gmail.com>
2020-11-25 00:30:56 -0600

    Creacion routes.js y  generar las rutas basicas #13

---
+++ frontend/src/routes.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Switch from "react";
+
+const Routes = () => {
+  return (
+    <Switch>
+      <Routes path="/">
+        <span>Home</span>
+      </Routes>
+    </Switch>
+  );
+};
+
+export default Routes;
