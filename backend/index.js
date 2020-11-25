ef9cb5660b9ebd021a8fced4fc1bcf8aabc548bb
ErickJoestar <erickborquez@gmail.com>
2020-11-24 21:44:19 -0700

    Add prettier and eslint #3

--- backend/index.js
+++ backend/index.js
@@ -8,11 +8,11 @@ const tempRoute = require("./routes/geis");
 const port = process.env.port || 3001;
 app.use(logger("dev"));
 app.use(cors());
-app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use("/geis", tempRoute);
 
-app.listen(port, function(){
-    console.log("Port: " + port);
-})
+app.listen(port, function () {
+  console.log("Port: " + port);
+});
 module.exports = app;
--- backend/routes/geis.js
+++ backend/routes/geis.js
@@ -4,17 +4,16 @@ const router = express.Router();
 let geis = require("../temporalDatabase");
 
 router.get("/list", async (req, res) => {
-    try {
-      res.status(200).json({
-        data: geis
-      });
-    } catch (err) {
-      res.status(400).json({
-        message: "Some error occured",
-        err
-      });
-    }
-  });
+  try {
+    res.status(200).json({
+      data: geis,
+    });
+  } catch (err) {
+    res.status(400).json({
+      message: "Some error occured",
+      err,
+    });
+  }
+});
 
-
-  module.exports = router;
+module.exports = router;
