600f6e0985d464a47c1c9f280f65106d5bb025b5
Hiromi00 <55908313+hiromi00@users.noreply.github.com>
2020-12-01 21:41:28 -0600

    Database creada #24

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,20 @@ const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const logger = require("morgan");
+const mongoose = require("mongoose");
+require('dotenv').config();
+
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.connect(process.env.MONGO_URI, {
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useCreateIndex: true
+  }).then( () => console.log("DB connected!"))
+  .catch(err => console.error(err))
+
 
 const HttpError = require("./models/http-error");
 const port = process.env.port || 3001;
